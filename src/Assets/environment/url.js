516ddc1406a4db994ff0b3e7f8227fe805d85a2a
zahid-01 <erzahidkhan101@gmail.com>
2023-09-12 18:25:46 +0530

    AWS

--- src/Assets/environment/url.js
+++ src/Assets/environment/url.js
@@ -1,5 +1,5 @@
 // export const URL = "http://localhost:5000/";
-// export const URL = "https://titanic-api.onrender.com/";
-export const URL =
-  "http://titanic-backend-dev.eba-4efmtmya.ap-south-1.elasticbeanstalk.com/";
+export const URL = "https://titanic-api.onrender.com/";
+// export const URL =
+// "http://titanic-backend-dev.eba-4efmtmya.ap-south-1.elasticbeanstalk.com/";
 // export const URL = "https://zany-gray-basket-clam-sari.cyclic.app/";
