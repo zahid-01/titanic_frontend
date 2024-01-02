cee5869fba4a74df42faac353a806e5d230b594c
Basit-Bashir <wbhat56@gmail.com>
2024-01-02 17:20:40 +0530

    updated footer

--- src/components/Footer/FooterPart.js
+++ src/components/Footer/FooterPart.js
@@ -17,7 +17,7 @@ const FooterPart = () => {
   return (
     <>
       <footer className="bg-white relative">
-        <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+        <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-10 sm:px-6 lg:space-y-10 lg:px-8">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
             <div>
               <div className="text-teal-600">
