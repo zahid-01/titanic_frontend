a590512fa260b20e6c6d2513e9ca8adbce8f06e2
Basit-Bashir <wbhat56@gmail.com>
2024-01-11 19:22:38 +0530

    updated URL

--- src/Assets/environment/url.js
+++ src/Assets/environment/url.js
@@ -1,2 +1,3 @@
 // export const URL = "http://localhost:5000/";
-export const URL = "https://titanic-api.onrender.com/";
+// export const URL = "https://titanic-api.onrender.com/";
+export const URL = "https://titanic-api-410913.de.r.appspot.com/";
